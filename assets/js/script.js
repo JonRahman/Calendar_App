8009e89b67397b790abdf0495b88a98315638305
JonRahman <jonrahman90@gmail.com>
2023-12-10 21:53:17 +0000

    Color-coded timeblocks, user can enter an event, event saves and persists.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,4 @@
 
-function hourColour() {
-
-}
-
 function updateTime() {
     const dateElement = $('#date');
     const timeElement = $('#time');
@@ -12,19 +8,27 @@ function updateTime() {
     timeElement.text(currentTime);
 }
 
+$(".time-block").each(function(){
+    var hourKey=$(this).attr("id")
+    var currentHour=dayjs().hour()
+    //var currentHour = 10
+    var rowHour=parseInt(hourKey)
+    console.log(currentHour,rowHour)
+    if(currentHour > rowHour) {
+        $(this).children("textarea").addClass("past");
+    } else if(currentHour === rowHour) {
+        $(this).children("textarea").addClass("present");
+    } else {
+        $(this).children("textarea").addClass("future");
+    }
+    $(this).children("textarea").val(localStorage.getItem(hourKey))
+})
 
-
-function timeContainerEntry() {
-
-}
-
-function timeContainerColor() {
-
-}
-
-function saveButton() {
-
-}
+$(".saveBtn").click(function(){
+    var hourKey = $(this).siblings(".col-8").attr("id")
+    var hourValue = $(this).siblings(".col-8").val()
+    localStorage.setItem(hourKey,hourValue)
+}) 
 
 setInterval(updateTime, 1000);
 
